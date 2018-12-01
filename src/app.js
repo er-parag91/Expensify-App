06f661e744ac59d14428096d71bed70a3778aec1
Parag <paragpatel91@gmail.com>
2018-12-01 11:08:05 -0600

    Getting started

--- src/app.js
+++ src/app.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import AppRouter from './routers/appRouter';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<p>This is my app!!</p>, document.getElementById('app'));
+
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+)
+
+const AddExpensePage = () => (
+    <div>
+        This is from my AddExpensePage component
+    </div>
+)
+
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact component={ExpenseDashboardPage}></Route>
+            <Route path="/create" component={AddExpensePage}></Route>
+        </div>
+    </BrowserRouter>)
+
+ReactDOM.render(routes, document.getElementById('app'));
