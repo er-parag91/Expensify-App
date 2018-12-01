7f1d0b146ab2c191d840584d4767dd638b286093
Parag <paragpatel91@gmail.com>
2018-12-01 10:05:45 -0600

    Second commit

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRouter from './routers/appRouter';
 
-ReactDOM.render(<p>This is my app</p>, document.getElementById('app'));
+ReactDOM.render( < p > This is my app!! < /p>, document.getElementById('app'));
