b92cf3cccdf6e87a824c8d970166561bb3a2abf1
Parag <paragpatel91@gmail.com>
2018-12-01 11:42:17 -0600

    Added routing Component

--- src/app.js
+++ src/app.js
@@ -2,28 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component
-    </div>
-)
-
-const AddExpensePage = () => (
-    <div>
-        This is from my AddExpensePage component
-    </div>
-)
-
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path="/" exact component={ExpenseDashboardPage}></Route>
-            <Route path="/create" component={AddExpensePage}></Route>
-        </div>
-    </BrowserRouter>)
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(AppRouter, document.getElementById('app'));
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" exact activeClassName="is-active">ExpenseDashboardPage</NavLink>
+        <NavLink to="/create" activeClassName="is-active">AddExpensePage</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">EditExpensePage</NavLink>
+        <NavLink to="/help" activeClassName="is-active">HelpPage</NavLink>
+    </header>
+)
+
+export default Header;
